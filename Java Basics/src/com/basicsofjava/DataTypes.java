ccd547221fa24438af0a54021b81526a9e01beff
ratna <gudipudiratnakumar@gmail.com>
2024-03-10 22:13:38 +0530

    Data Types is added

---
+++ Java Basics/src/com/basicsofjava/DataTypes.java
@@ -0,0 +1,23 @@
+package com.basicsofjava;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b=12;
+        short s=1200;
+        int i=567890;
+        long    l=234567898L;
+        float f=3.4f;
+        double d=345.66d;
+        char c='A';
+        boolean n=false;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(n);
+
+    }
+}
